b424d4c1f8a7dc9c69d6ac3b114f7fac13688e19
RahulHi <67343291+RahulHi@users.noreply.github.com>
2023-09-17 22:19:17 -0500

    Star Wars data added.

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import { request, gql, GraphQLClient } from 'graphql-request';
 
 const app = express()
 
@@ -7,10 +8,46 @@ app.use(cors({
     origin: "http://localhost:5500"
 }))
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+    const graphQLClient = new GraphQLClient('https://swapi-graphql.netlify.app/.netlify/functions/index', {
+        headers: {
+        },
+    });
+    const query = gql`
+        {
+            allSpecies {
+                species {
+                    averageLifespan
+                    averageHeight
+                    name
+                }
+            }
+        }
+    `;
+    const results = await graphQLClient.request(query);
+
+    console.log(JSON.stringify(results, undefined, 2));
+
+    var data = [];
+
+    for (const species of results.allSpecies.species) {
+        var trace = {
+            x: [species.averageLifespan],
+            y: [species.averageHeight],
+            mode: 'markers',
+            name: species.name
+        };
+
+        data.push(trace);
+    }
+
+    var layout = {
+        title:'Average height (y) and lifespan (x) of different Star Wars species'
+    };
+
     res.send({
-        x: [1, 2, 3],
-        y: [4, 5, 6]
+        data, 
+        layout
     }).status(200)
 })
 
